d21d520fbcaf7fbccca6d44b8168f3e5a1b3e36a
migtarx <migtarx@gmail.com>
2022-07-22 12:52:54 +0200

    feat: cdn server complete

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const Notion = require('./notion');
+require('dotenv').config();
+const express = require('express');
+const colors = require('colors');
+const path = require('path');
+const app = express();
+const arguments = process.argv[2];
+const PORT = arguments == "-t" ? 7000 : arguments == "-p" ? 6026 : 8000;
 
-(async () => {
-    //console.log(await Notion.getUsersIdsByGroupId('64057f9df71241719b7162265c8bb70f'));
-    console.log(await Notion.getGroupsByUserId('889980333292945478'))
-})();
+app.get('/:apikey', async(req, res) => {
+    if (req.params.apikey != process.env.APIKEY) return res.sendStatus(403)
+    res.sendFile(path.join(__dirname, '/lib/notion.js'));
+})
 
+app.listen(PORT, () => {
+    console.log(`Now listening to requests on port ` + `${PORT}`.cyan);
+    console.log(`Access by ` + `http://127.0.0.1:${PORT} (${PORT == 7000 ? "test" : PORT == 8000 ? "dev": "prod"})`.yellow)
+});
